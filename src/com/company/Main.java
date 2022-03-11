b2c25a34e8e9929d12dd91746f2dae41aa6e9b68
PolMasi <pol.masi@gmail.com>
2022-03-11 13:31:21 +0100

    hh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,32 @@ package com.company;
 
 import com.company.business.BusinessFacade;
 import com.company.business.BusinessFacadeImpl;
+import com.company.business.TrialMaster;
+import com.company.business.TrialPublicacionArticulo;
 import com.company.persistence.TrialDAO;
 import com.company.persistence.TrialDAOcsv;
+import com.company.persistence.TrialDAOjson;
 import com.company.presentation.ConsoleUIManager;
 import com.company.presentation.UIController;
 import com.company.presentation.UIManager;
+import org.w3c.dom.CDATASection;
 
 public class Main {
 
     public static void main(String[] args) {
         UIManager consoleUIManager = new ConsoleUIManager();
-        //menu.ejecutarMenu();
         TrialDAO trialDAO = new TrialDAOcsv("data/trials.csv");
-        //trialDAO.getAll();
         BusinessFacade businessFacade = new BusinessFacadeImpl();
         UIController uiController = new UIController(consoleUIManager, businessFacade);
+        TrialDAOjson trialDAOjson = new TrialDAOjson("data/trials.json");
+        TrialPublicacionArticulo trialPublicacionArticulojson = new TrialPublicacionArticulo("fdd", "dd",
+                "d3", 36, 75, 67);
+        trialDAOjson.save(trialPublicacionArticulojson);
+        System.out.println(trialDAOjson.getPublicacion(0).getNombreRevista());
+        TrialMaster trialMaster = new TrialMaster("namemaster", "ddd", 70, 30);
+        trialDAOjson.save(trialMaster);
+        trialDAOjson.save(trialMaster);
+        System.out.println(trialDAOjson.getMaster(1).getMasterName());
         uiController.run();
 
     }
--- src/com/company/persistence/TrialDAOjson.java
+++ src/com/company/persistence/TrialDAOjson.java
@@ -1,4 +1,181 @@
 package com.company.persistence;
 
-public class TrialDAOjson {
+import com.company.business.*;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class TrialDAOjson implements TrialDAO {
+    private String path;
+
+    public TrialDAOjson(String path) {
+        try {
+            Path p = Paths.get(path);       //ruta donde guardamos los archivos
+            if (!Files.exists(p)) {
+                Files.createFile(p);
+            }
+            this.path = path;
+
+        } catch (IOException | InvalidPathException e) {
+            e.getMessage();
+        }
+    }
+
+    @Override
+    public void save(TrialPublicacionArticulo trial) {
+        try {
+            JsonElement trialjson;
+            Reader file = null;
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            ArrayList trials = new ArrayList<>();
+            JsonReader reader = null;
+
+            trials.add(trial);
+            
+            try {
+                file = new FileReader(path);
+                reader = new JsonReader(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            if(!(file.read() == -1)) {
+                trials.add(gson.fromJson(reader, Object.class));
+            }
+
+            trialjson = gson.toJsonTree(trials);
+            FileWriter fileWriter = new FileWriter(path, false);
+            fileWriter.write(gson.toJson(trialjson));
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(TrialMaster trial) {
+        try {
+            JsonElement trialjson;
+            Reader file = null;
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            ArrayList trials = new ArrayList<>();
+            JsonReader reader = null;
+
+            trials.add(trial);
+
+            try {
+                file = new FileReader(path);
+                reader = new JsonReader(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            if(!(file.read() == -1)) {
+                trials.add(gson.fromJson(reader, Object.class));
+            }
+
+            System.out.println(trials.size());
+
+            trialjson = gson.toJsonTree(trials);
+            FileWriter fileWriter = new FileWriter(path, false);
+            fileWriter.write(gson.toJson(trialjson));
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(TrialTesis trial) {
+
+    }
+
+    @Override
+    public void save(TrialBudget trial) {
+
+    }
+
+    @Override
+    public Trial[] getAll() {
+        return new Trial[0];
+    }
+
+    @Override
+    public TrialPublicacionArticulo getPublicacion(int numberTrial) {
+        Reader file = null;
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ArrayList trials = new ArrayList<>();
+        JsonReader reader = null;
+
+        try {
+            file = new FileReader(path);
+            reader = new JsonReader(file);
+            if(!(file.read() == -1)) {
+                trials.add(gson.fromJson(reader, TrialPublicacionArticulo.class));
+            }
+        } catch (java.io.IOException e ) {
+            e.printStackTrace();
+        }
+
+        return (TrialPublicacionArticulo) trials.get(numberTrial);
+    }
+
+    @Override
+    public TrialMaster getMaster(int numberTrial) {
+        Reader file = null;
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ArrayList trials = new ArrayList<>();
+        JsonReader reader = null;
+
+        try {
+            file = new FileReader(path);
+            reader = new JsonReader(file);
+            if(!(file.read() == -1)) {
+                trials.add(gson.fromJson(reader, TrialMaster.class));
+            }
+        } catch (java.io.IOException e ) {
+            e.printStackTrace();
+        }
+
+        return (TrialMaster) trials.get(numberTrial);
+    }
+
+    @Override
+    public TrialTesis getTesis(int numberTrial) {
+        return null;
+    }
+
+    @Override
+    public TrialBudget getBudget(int numberTrial) {
+        return null;
+    }
+
+    @Override
+    public boolean trialExit(int numPrueba) {
+        return false;
+    }
+
+    @Override
+    public void delete(int numPrueba) {
+
+    }
+
+    @Override
+    public int getType(int numPrueba) {
+        return 0;
+    }
 }
